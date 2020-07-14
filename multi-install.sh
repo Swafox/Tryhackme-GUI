6a35f79f564b53e40f331e9342664c0da6f3e259
Swafox <25106596+Swafox@users.noreply.github.com>
2020-07-14 15:44:37 +0300

    Initial

---
+++ multi-install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "+-+-+-+-+-+-+-+-+-+ +-+-+-+ +-+-+-+-+-+-+-+-+-+ \n|T|r|y|H|a|c|k|M|e| |G|U|I| |I|n|s|t|a|l|l|e|r| \n+-+-+-+-+-+-+-+-+-+ +-+-+-+ +-+-+-+-+-+-+-+-+-+"
+echo ""
+
+mkdir tryhackme && cd tryhackme
+
+wget https://i.imgur.com/HiIWSQq.png -O icon.png
+
+nativefier -p linux -a x64 -i icon.png --disable-context-menu --disable-dev-tools --single-instance --internal-urls ".*" https://tryhackme.com/
+
+mv TryHackMeLearnCybersecurity-linux-x64 TryHackMe && cd TryHackMe && mv TryHackMeLearnCybersecurity TryHackMe 
+
+chmod 777 TryHackMe
+
+./TryHackMe
